f3643cbf67f40e2f9e68669a8039310966f36f02
Justin <justingalura@gmail.com>
2021-08-26 17:58:03 +0800

    database and model

--- app/src/main/java/com/mobdeve/s11/gelvoleo/galura/journal/model/Entry.java
+++ app/src/main/java/com/mobdeve/s11/gelvoleo/galura/journal/model/Entry.java
@@ -1,11 +1,68 @@
 package com.mobdeve.s11.gelvoleo.galura.journal.model;
 
-public class Entry {
-    private String title, caption;
+import java.util.Date;
+import java.util.UUID;
 
-    public Entry(String title, String caption){
+public class Entry {
+
+    private UUID id;
+    private String title;
+    private String caption;
+    private Date date;
+    private int archived;
+    private String filename;
+
+    public Entry(String title, String caption) {
         this.title = title;
         this.caption = caption;
+    }
+
+    public Entry(String title, String caption, Date date){
+        this.id = UUID.randomUUID();
+        this.title = title;
+        this.caption = caption;
+        this.date = date;
+        this.archived = 0;
+    }
+
+    public Entry(UUID uuid) {
+        this.id = uuid;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int isArchived() {
+        return archived;
+    }
+
+    public void setArchived(int archived) {
+        this.archived = archived;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 
     public String getTitle(){
---
+++ app/src/main/java/com/mobdeve/s11/gelvoleo/galura/journal/model/EntryCursorWrapper.java
@@ -0,0 +1,34 @@
+package com.mobdeve.s11.gelvoleo.galura.journal.model;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+
+import java.util.Date;
+import java.util.UUID;
+
+import static com.mobdeve.s11.gelvoleo.galura.journal.model.EntryDbSchema.*;
+
+public class EntryCursorWrapper extends CursorWrapper {
+
+    public EntryCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+    public Entry getEntry() {
+        String uuidString = getString(getColumnIndex(EntryTable.Cols.UUID));
+        String title = getString(getColumnIndex(EntryTable.Cols.TITLE));
+        String caption = getString(getColumnIndex(EntryTable.Cols.CAPTION));
+        long date = getLong(getColumnIndex(EntryTable.Cols.DATE));
+        int isArchived = getInt(getColumnIndex(EntryTable.Cols.ARCHIVED));
+        String filename = getString(getColumnIndex(EntryTable.Cols.FILENAME));
+
+        Entry entry = new Entry(UUID.fromString(uuidString));
+        entry.setTitle(title);
+        entry.setCaption(caption);
+        entry.setDate(new Date(date));
+        entry.setArchived(isArchived);
+        entry.setFilename(filename);
+
+        return entry;
+    }
+}
