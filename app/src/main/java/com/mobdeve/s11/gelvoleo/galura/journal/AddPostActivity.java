d8a32ae83f1c3bc2cde6faf8b6eace2054b25a1d
WaterproofLoaf <gelvoleo.michele.b@gmail.com>
2021-08-26 16:55:28 +0800

    Added journal entry functionality

---
+++ app/src/main/java/com/mobdeve/s11/gelvoleo/galura/journal/AddPostActivity.java
@@ -0,0 +1,46 @@
+package com.mobdeve.s11.gelvoleo.galura.journal;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddPostActivity extends AppCompatActivity {
+    private EditText etTitle;
+    private EditText etCaption;
+    private Button btnSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_entry);
+
+        this.etTitle = findViewById(R.id.et_add_title);
+        this.etCaption = findViewById(R.id.et_add_caption);
+        this.btnSave = findViewById(R.id.btn_add_save);
+
+        this.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(etTitle.getText().toString().isEmpty()){
+                    Toast.makeText(AddPostActivity.this, "Your Journal Title", Toast.LENGTH_SHORT).show();
+                }
+
+                if(etCaption.getText().toString().isEmpty()){
+                    Toast.makeText(AddPostActivity.this, "Let's get typing!", Toast.LENGTH_SHORT).show();
+                }
+
+                Entry entry = new Entry(
+                        etTitle.getText().toString(),
+                        etCaption.getText().toString());
+
+                assert  DataHelper.getData() != null;
+                DataHelper.getData().add(0, entry);
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/mobdeve/s11/gelvoleo/galura/journal/MainActivity.java
+++ app/src/main/java/com/mobdeve/s11/gelvoleo/galura/journal/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 
@@ -39,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
         entriesAdapter = new EntriesAdapter(this.entries);
 
         this.fabAdd = findViewById(R.id.fab_main_add);
+
+        fabAdd.setOnClickListener(view ->{
+            Intent intent = new Intent(getBaseContext(), AddPostActivity.class);
+            startActivity(intent);
+        });
 
         this.rvEntries = findViewById(R.id.rvEntries);
         this.rvEntries.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
