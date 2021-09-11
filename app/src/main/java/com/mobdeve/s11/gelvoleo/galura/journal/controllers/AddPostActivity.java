ce51f61d63758ade02cc12dcd67ad2e249d57dc9
WaterproofLoaf <gelvoleo.michele.b@gmail.com>
2021-09-11 23:30:40 +0800

    Added data picker.

--- app/src/main/java/com/mobdeve/s11/gelvoleo/galura/journal/controllers/AddPostActivity.java
+++ app/src/main/java/com/mobdeve/s11/gelvoleo/galura/journal/controllers/AddPostActivity.java
@@ -2,12 +2,17 @@ package com.mobdeve.s11.gelvoleo.galura.journal.controllers;
 
 import android.app.DatePickerDialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.mobdeve.s11.gelvoleo.galura.journal.model.EntryLab;
@@ -16,44 +21,68 @@ import com.mobdeve.s11.gelvoleo.galura.journal.utils.DataHelper;
 import com.mobdeve.s11.gelvoleo.galura.journal.model.Entry;
 import com.mobdeve.s11.gelvoleo.galura.journal.R;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class AddPostActivity extends AppCompatActivity {
+    private static final String TAG = "AddPostActivity";
+
     DatePickerDialog picker;
-    private EditText etDate;
+    private TextView tvDate;
     private EditText etTitle;
     private EditText etCaption;
     private FloatingActionButton fabSave;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_entry);
 
-        this.etDate = findViewById(R.id.et_add_date);
+        this.tvDate = findViewById(R.id.tv_add_date);
         this.etTitle = findViewById(R.id.et_add_title);
         this.etCaption = findViewById(R.id.et_add_caption);
         this.fabSave = findViewById(R.id.fab_add_save);
 
+        final Calendar calendar = Calendar.getInstance();
+
         Context context = this;
 
-        this.fabSave.setOnClickListener(view -> {
+        tvDate.setOnClickListener(v -> {
 
-            final Calendar calendar = Calendar.getInstance();
             int day = calendar.get(Calendar.DAY_OF_MONTH);
             int month = calendar.get(Calendar.MONTH);
             int year = calendar.get(Calendar.YEAR);
 
-//            picker = new DatePickerDialog(AddPostActivity.this,
-//                    new DatePickerDialog.OnDateSetListener() {
-//                        @Override
-//                        public void onDateSet(DatePicker view, int year, int month, int dayh) {
-//                            etDate.setText(day + "/" + (month + 1) + "/" + year);
-//                        }
-//                    }, year, month, day);
-//            picker.show();
+            DatePickerDialog dialog = new DatePickerDialog(
+                    AddPostActivity.this,
+                    //android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                    android.R.style.Theme_Holo_Dialog_MinWidth,
+                    mDateSetListener,
+                    year, month, day);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.show();
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                SimpleDateFormat sdf = new SimpleDateFormat("d MMM yyyy");
+                calendar.set(year, month, day);
+                String date = sdf.format(calendar.getTime());
+
+                //month = month + 1;
+                //Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+                //String date = month + "/" + day + "/" + year;
+
+                tvDate.setText(date);
+            }
+        };
+
+        this.fabSave.setOnClickListener(view -> {
 
             if(etTitle.getText().toString().isEmpty()){
                 Toast.makeText(AddPostActivity.this, "Title Required", Toast.LENGTH_SHORT).show();
